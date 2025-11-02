c3295d41b75b5ffef65bcce72ec59eda603669e2
VLAD F <woohda@ya.ru>
2025-11-02 17:26:52 +0700

    добавил файлы prisma

---
+++ lib/prisma.ts
@@ -0,0 +1,26 @@
+import { PrismaClient } from '@prisma/client';
+import { withAccelerate } from '@prisma/extension-accelerate';
+
+import env from '../app/utils/env/env';
+
+/**
+ * Эта функция создает экземпляр PrismaClient с расширением Accelerate.
+ * Она использует глобальную переменную для хранения экземпляра,
+ * чтобы избежать создания нескольких экземпляров в среде разработки.
+ * В производственной среде создается новый экземпляр.
+ * @property {PrismaClient} prisma - экземпляр PrismaClient
+ * @property {globalForPrisma} globalForPrisma - глобальная переменная для хранения экземпляра PrismaClient
+ */
+
+const globalForPrisma = globalThis as unknown as {
+	prisma: PrismaClient;
+};
+
+const prisma
+	= globalForPrisma.prisma
+		|| (new PrismaClient().$extends(withAccelerate()) as unknown as PrismaClient);
+
+if (env.NODE_ENV !== 'production')
+	globalForPrisma.prisma = prisma;
+
+export default prisma;
