370f69625d8f9144e01589ffd8bcd11d973f791f
VLAD F <woohda@ya.ru>
2025-11-04 19:06:07 +0700

    Новое: добавил компонент Toast из библиотеки shadcn

---
+++ app/components/ui/toast/ToastProvider.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import type { ToastProviderProps } from 'reka-ui';
+
+import { ToastProvider } from 'reka-ui';
+
+const props = defineProps<ToastProviderProps>();
+</script>
+
+<template>
+	<ToastProvider v-bind="props">
+		<slot />
+	</ToastProvider>
+</template>
