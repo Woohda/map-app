6844561a12d3f550935ed931c49882a54e6c26ac
VLAD F <woohda@ya.ru>
2025-11-08 20:51:55 +0700

    Изменил: поправил адреса ссылок

--- app/components/app/NavBar.vue
+++ app/components/app/NavBar.vue
@@ -10,10 +10,10 @@
 			MAP_APP
 		</NuxtLink>
 		<div class="flex gap-5">
-			<NuxtLink to="/signin" class="hover:text-primary transition-colors duration-200 focus-visible:border-ring focus-visible:ring-ring/60 focus-visible:ring-[3.5px] rounded-sm outline-none">
+			<NuxtLink to="/signin" class="visited:text-current hover:text-primary transition-colors duration-200 focus-visible:border-ring focus-visible:ring-ring/60 focus-visible:ring-[3.5px] rounded-sm outline-none">
 				Sign In
 			</NuxtLink>
-			<NuxtLink to="/signup" class="hover:text-primary transition-colors duration-200 focus-visible:border-ring focus-visible:ring-ring/60 focus-visible:ring-[3.5px] rounded-sm outline-none">
+			<NuxtLink to="/signup" class="visited:text-current hover:text-primary transition-colors duration-200 focus-visible:border-ring focus-visible:ring-ring/60 focus-visible:ring-[3.5px] rounded-sm outline-none">
 				Sign Up
 			</NuxtLink>
 		</div>
