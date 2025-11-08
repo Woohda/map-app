4946c2f1adacd80cba6a7547b904ea033fffb63c
VLAD F <woohda@ya.ru>
2025-11-08 16:38:10 +0700

    Новое: добавил компонент лоадера "Точки"

---
+++ app/components/ui/loader/DotsLoader.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import type { HTMLAttributes } from 'vue';
+
+import { cn } from '~/utils/utils';
+
+const props = defineProps<{
+	class?: HTMLAttributes['class'];
+}>();
+</script>
+
+<template>
+	<span aria-label="Loading" role="status" :class="cn('inline-flex space-x-1', props.class)">
+		<span class="w-2 h-2 bg-current rounded-full animate-bounce delay-150" />
+		<span class="w-2 h-2 bg-current rounded-full animate-bounce delay-300" />
+		<span class="w-2 h-2 bg-current rounded-full animate-bounce delay-450" />
+	</span>
+</template>
