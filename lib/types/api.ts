b44f6208e1556d5f8d62a49d04741b7287879827
VLAD F <woohda@ya.ru>
2025-11-09 16:04:58 +0700

    Удалил: удалил типизацию запросов авторизации и регистрации, использую тип Lucia

--- lib/types/api.ts
+++
@@ -1,6 +0,0 @@
-import type { User } from 'lucia';
-
-// Универсальный ответ для запросов авторизации
-export type AuthResponse
-	= | { user: User }
-		| { fieldErrors?: Record<string, string>; generalError?: string };
