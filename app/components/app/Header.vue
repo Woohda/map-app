a791fb9def005c8b1d619d02972537544ab19af2
VLAD F <woohda@ya.ru>
2026-02-27 16:20:46 +0700

    Изменил: добавил новые компоненты на главную страницу, поменял стили и разметку main, изменил стили header

--- app/components/app/Header.vue
+++ app/components/app/Header.vue
@@ -7,7 +7,7 @@ import ThemeToggle from '~/components/button/ThemeToggle.vue';
 </script>
 
 <template>
-	<header class="mx-auto max-w-7xl flex items-center grow gap-5 p-5">
+	<header class="mx-auto max-w-7xl w-full flex items-center grow gap-5 p-5">
 		<NavBar />
 		<ThemeToggle />
 	</header>
--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -2,14 +2,22 @@
     setup
     lang="ts"
 >
+import AppSidebar from '~/components/app/AppSidebar.vue';
 import Header from '~/components/app/Header.vue';
 </script>
 
 <template>
 	<Header />
-	<slot />
+	<SidebarProvider :default-open="true">
+		<main class="flex items-center justify-center">
+			<AppSidebar />
+			<SidebarTrigger>
+				<Icon name="tabler:menu" style="width: 20px; height: 20px;" />
+			</SidebarTrigger>
+			<slot />
+		</main>
+	</SidebarProvider>
 </template>
 
 <style scoped>
-
 </style>
