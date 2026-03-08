4e1312ecc0ae9d936193745c40e0ea138c3315f6
VLAD F <woohda@ya.ru>
2026-03-08 13:18:01 +0700

    Изменения: попраивил стили сайдбара, сделал тригер для мобильной версии в Header.vue

--- app/components/app/Header.vue
+++ app/components/app/Header.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
 import NavBar from '~/components/app/NavBar.vue';
 import ThemeToggle from '~/components/button/ThemeToggle.vue';
+import { useSidebar } from '~/components/ui/sidebar';
+import SidebarTriggerMobile from '~/components/ui/sidebar/SidebarTriggerMobile.vue';
 
-const route = useRoute();
+const { isMobile } = useSidebar();
 </script>
 
 <template>
@@ -10,11 +12,10 @@ const route = useRoute();
 		class="absolute top-0 z-40 w-full border-b bg-background/80 backdrop-blur supports-backdrop-filter:bg-background/60"
 	>
 		<div
-			class="mx-auto flex h-16 w-full max-w-7xl items-center gap-3 px-4 md:px-6"
+			class="mx-auto flex h-16 w-full max-w-7xl items-center justify-between gap-3 md:px-6"
 		>
-			<SidebarTrigger
-				v-if="!['/sign-in', '/sign-up'].includes(route.path)"
-				class="shrink-0"
+			<SidebarTriggerMobile
+				v-if="isMobile"
 			/>
 			<div class="min-w-0 flex-1">
 				<NavBar />
