223828999646ce6ab4c139a5b2ea6c471cac0998
VLAD F <woohda@ya.ru>
2026-03-11 15:56:54 +0700

    Изменения: расширил функционал popup, теперь в него передается дата

--- lib/types/popup.ts
+++ lib/types/popup.ts
@@ -1,8 +1,10 @@
 export enum PopupType {
 	MARKER_INFO = 'markerInfo',
 	ADD_LOCATION = 'addLocation',
+	ERROR_INFO = 'errorInfo',
 }
 
 export interface PopupState {
 	type: PopupType | null;
+	data?: unknown;
 }
