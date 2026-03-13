4bbde1b29131c55aee1de5648f4f2f5d4fd308ea
VLAD F <woohda@ya.ru>
2026-03-13 11:39:36 +0700

    Новое: создал layout для main страниц

---
+++ app/layouts/main.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import Header from '~/components/app/Header.vue';
+</script>
+
+<template>
+	<SidebarProvider
+		:default-open="true"
+		class="min-h-screen w-full"
+	>
+		<SidebarInset class="min-h-screen">
+			<Header />
+			<main class="min-h-screen pt-20">
+				<div class="w-full flex justify-center max-sm:px-5">
+					<slot />
+				</div>
+			</main>
+		</SidebarInset>
+	</SidebarProvider>
+</template>
+
+<style scoped></style>
