edd61323e6e4de3b50ff6e78be809fadca9ebbaf
VLAD F <woohda@ya.ru>
2026-04-26 00:21:08 +0700

    Изменения:

--- server/api/auth/logout.post.ts
+++ server/api/auth/logout.post.ts
@@ -1,6 +1,6 @@
 import type { H3Event } from 'h3';
 
-import { createNewSessionCookie, validateRequest } from '~server/utils/auth';
+import { createNewSessionCookie, lucia, validateRequest } from '~server/utils/auth';
 
 export default defineEventHandler(async (event: H3Event) => {
 	const { session } = await validateRequest(event);
