ed87c5e798006f2cc26aede5cd7cc3d38bd5c733
VLAD F <woohda@ya.ru>
2026-04-28 19:06:15 +0700

    Изменения: фиксил стилей расположения компонентов в Header.vue

--- app/components/app/Header.vue
+++ app/components/app/Header.vue
@@ -21,15 +21,13 @@ const isMobile = useMediaQuery('(max-width: 768px)');
 			class="mx-auto flex h-16 w-full max-w-7xl items-center justify-between gap-3 px-2 md:px-6"
 		>
 			<SidebarTriggerMobile v-if="['/', '/profile'].includes(route.path)" />
-			<div class="flex items-center min-w-0">
-				<NuxtLink
-					to="/"
-					class="px-1 text-lg visited:text-current hover:text-primary transition-colors duration-200 focus-visible:border-ring focus-visible:ring-ring/60 focus-visible:ring-[3.5px] rounded-sm outline-none"
-				>
-					MAP_APP
-				</NuxtLink>
-				<NavBar v-if="!isMobile" />
-			</div>
+			<NuxtLink
+				to="/"
+				class="px-1 text-lg visited:text-current hover:text-primary transition-colors duration-200 focus-visible:border-ring focus-visible:ring-ring/60 focus-visible:ring-[3.5px] rounded-sm outline-none"
+			>
+				MAP_APP
+			</NuxtLink>
+			<NavBar v-if="!isMobile" class="ml-auto" />
 			<NuxtLink
 				v-if="isMobile && !isAuthenticated"
 				to="/sign-in"
